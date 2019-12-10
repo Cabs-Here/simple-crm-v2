1f86639cb08caa1d18a6b3e401df2c53a8fe2581
Ryan McKenzie <mckenzie.aviation@gmail.com>
2019-12-09 20:43:34 -0600

    added JWT authentication

---
+++ Classroom.SimpleCRM.SqlDbServices/CrmIdentityDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Reflection;
+
+namespace Classroom.SimpleCRM.SqlDbServices
+{
+    public class CrmIdentityDbContextFactory : IDesignTimeDbContextFactory<CrmIdentityDbContext>
+    {
+        public CrmIdentityDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<CrmIdentityDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleCrmV2;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsbuilder => optionsbuilder.MigrationsAssembly(typeof(CrmIdentityDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new CrmIdentityDbContext(builder.Options);
+        }
+    }
+}
--- Classroom.SimpleCRM.WebApi/Startup.cs
+++ Classroom.SimpleCRM.WebApi/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,13 +9,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Classroom.SimpleCRM.SqlDbServices;
-using Classroom.SimpleCRM.WebApi.Data;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Classroom.SimpleCRM.WebApi.Auth;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System;
 
 namespace Classroom.SimpleCRM.WebApi
 {
     public class Startup
     {
+        private const string SecretKey = "jkIke23Ldpwskdf73pBd"; //<-- NEW: make up a random key here
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,10 +33,6 @@ namespace Classroom.SimpleCRM.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSpaStaticFiles(config =>
-            {
-                config.RootPath = Configuration["SpaRoot"];
-            });
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -38,12 +40,84 @@ namespace Classroom.SimpleCRM.WebApi
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            var googleOptions = Configuration.GetSection(nameof(GoogleAuthSettings));
+            services.Configure<GoogleAuthSettings>(options =>
+            {
+                options.ClientId = googleOptions[nameof(GoogleAuthSettings.ClientId)];
+                options.ClientSecret = googleOptions[nameof(GoogleAuthSettings.ClientSecret)];
+            });
+
+            var microsoftOptions = Configuration.GetSection(nameof(MicrosoftAuthSettings));
+            services.Configure<MicrosoftAuthSettings>(options =>
+            {
+                options.ClientId = microsoftOptions[nameof(MicrosoftAuthSettings.ClientId)];
+                options.ClientSecret = microsoftOptions[nameof(MicrosoftAuthSettings.ClientSecret)];
+            });
+
+            services.AddSpaStaticFiles(config =>
+            {
+                config.RootPath = Configuration["SpaRoot"];
+            });
+
+            var jwtOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+            var tokenValidationPrms = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = jwtOptions[nameof(JwtIssuerOptions.IssuedAt)],
+                ValidateAudience = true,
+                ValidAudience = jwtOptions[nameof(JwtIssuerOptions.Audience)],
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationPrms;
+                configureOptions.SaveToken = true; // allows token access in controller
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiPolicy", policy => policy.RequireClaim(
+                    Constants.JwtClaimIdentifiers.Rol,
+                    Constants.JwtClaims.ApiAccess));
+            });
+
+            var identityBuilder = services.AddIdentityCore<CrmIdentityUser>(o =>
+            { // add any custom password rules here
+            });
+            identityBuilder = new IdentityBuilder(
+                identityBuilder.UserType,
+                typeof(IdentityRole),
+                identityBuilder.Services);
+            identityBuilder.AddEntityFrameworkStores<CrmIdentityDbContext>();
+            identityBuilder.AddRoleValidator<RoleValidator<IdentityRole>>();
+            identityBuilder.AddRoleManager<RoleManager<IdentityRole>>();
+            identityBuilder.AddSignInManager<SignInManager<CrmIdentityUser>>();
+            identityBuilder.AddDefaultTokenProviders();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<CrmIdentityDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<CrmDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IJwtFactory, JwtFactory>();
             services.AddScoped<ICustomerData, SqlCustomerData>();
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
             services.AddScoped<IUrlHelper, UrlHelper>(factory =>
@@ -51,10 +125,6 @@ namespace Classroom.SimpleCRM.WebApi
                 var context = factory.GetService<IActionContextAccessor>().ActionContext;
                 return new UrlHelper(context);
             });
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -82,19 +152,23 @@ namespace Classroom.SimpleCRM.WebApi
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "identity",
+                    template: "Identity/{controller=Account}/{action=Register}/{id?}");
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
             app.UseWhen(
-      context => !context.Request.Path.StartsWithSegments("/api"),
-      appBuilder => appBuilder.UseSpa(spa =>
-      {
-          spa.Options.SourcePath = "../simple-crm-cli";
-          if (env.IsDevelopment())
-          {
-              spa.UseAngularCliServer(npmScript: "start");
-          }
-      }));
+                context => !context.Request.Path.StartsWithSegments("/api"),
+                appBuilder => appBuilder.UseSpa(spa =>
+                {
+                    spa.Options.SourcePath = "../simple-crm-cli";
+                    if (env.IsDevelopment())
+                    {
+                        spa.UseAngularCliServer(npmScript: "start");
+                    }
+                }));
         }
     }
 }
